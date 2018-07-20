6c77458360509b67c6c26ccf37ec66b76ad71941
杜大江 <dudajiang@boco.com.cn>
2018-07-20 14:52:37 +0800

    update 1

--- src/controller/zentao.js
+++ src/controller/zentao.js
@@ -6,7 +6,8 @@ module.exports = class extends Base {
     console.log(sql);
     let model = think.model('bi_report_amb_story');
     let data = await model.select();
-    console.log(JSON.stringify(data));
+    // console.log(JSON.stringify(data));
+    
   }
 
 };
